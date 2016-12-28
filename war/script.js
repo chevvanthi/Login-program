e68699b7e5013816de3b82fad79670c5c7586bd7
chevvanthi <chevvanthi.ezhilarasu@a-cti.com>
2016-12-28 17:49:16 +0530

    Adding commit messages

--- war/script.js
+++ war/script.js
@@ -30,7 +30,6 @@ var namearr=[];
 	
 	console.log("added something");
 	console.log("Added again here for test");
-	console.log("will be added in staging area");
 
 	function load() {
 		document.getElementById("name").focus();}
